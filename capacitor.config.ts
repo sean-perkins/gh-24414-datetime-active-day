84bd3835d7c7049e021a045d487bdd90d6c6798b
Sean Perkins <sean@ionic.io>
2021-12-22 11:47:06 -0500

    chore(): reproduce issue

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gh-24414-datetime-active-day',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
